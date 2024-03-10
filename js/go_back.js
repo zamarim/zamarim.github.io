f9cf2b9e85cbfe8d36f1efae6d37ec36024e2659
AdamHoles <AdamHoles@gMail.com>
2024-03-10 09:29:05 +0100

    I'm going to e-church.

--- js/go_back.js
+++ js/go_back.js
@@ -1,3 +1,28 @@
 document.getElementById("go_back").addEventListener("click", () => {
 	history.back();
 });
+
+let ascii = document.querySelector(".ASCII_approximation > dt");
+console.log(ascii);
+
+let עם_אותיות = ascii.innerText
+console.log(עם_אותיות);
+console.log( עם_אותיות.charAt(0) );
+
+let with_ascii = document.querySelector(".ASCII_approximation > dd");
+console.log(with_ascii);
+
+const ot2ascii = (ot) => {
+	if ("ש" === ot)
+		return "Sh";
+}
+
+console.log( ot2ascii(עם_אותיות.charAt(0)) );
+
+const otiot2ascii = str => {
+	console.log(str.length);
+	for (let i = 0; i <= str.length - 1; ++i )
+		console.log(i);
+}
+
+otiot2ascii( עם_אותיות );
